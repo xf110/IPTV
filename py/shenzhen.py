4bea61016d8c761a3433af481ebec10247098e13
xf110 <59716770+xf110@users.noreply.github.com>
2024-05-09 15:45:19 +0800

    Create shenzhen.py

---
+++ py/shenzhen.py
@@ -0,0 +1,87 @@
+import time
+import os
+import concurrent.futures
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import re
+
+urls = [
+    "https://fofa.info/result?qbase64=dWRweHkgJiZjaXR5PWh1aXpob3U%3D",  # 惠州
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjaXR5PSJndWFuZ3pob3Ui"  # 广州
+    
+    ]
+
+
+# 修改URL的函数
+def modify_urls(url):
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "status"
+    modified_urls = [f"{base_url}{ip_address[:-1]}{i}{port}{ip_end}" for i in range(1, 256)]
+    return modified_urls
+
+# 检查URL是否可访问的函数
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=0.5)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        return None
+
+results = []
+
+
+
+for url in urls:
+    try:
+        # 创建一个Chrome WebDriver实例
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+    
+        driver = webdriver.Chrome(options=chrome_options)
+        # 使用WebDriver访问网页
+        driver.get(url)  # 将网址替换为你要访问的网页地址
+        time.sleep(10)
+        # 获取网页内容
+        page_content = driver.page_source
+    
+        # 关闭WebDriver
+        driver.quit()
+    
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        x_urls = []
+        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+            url = url.strip()
+            ip_start_index = url.find("//") + 2
+            ip_end_index = url.find(":", ip_start_index)
+            ip_dot_start = url.find(".") + 1
+            ip_dot_second = url.find(".", ip_dot_start) + 1
+            ip_dot_three = url.find(".", ip_dot_second) + 1
+            base_url = url[:ip_start_index]  # http:// or https://
+            ip_address = url[ip_start_index:ip_dot_three]
+            port = url[ip_end_index:]
+            ip_end = "1"
+            modified_ip = f"{ip_address}{ip_end}"
+            x_url = f"{base_url}{modified_ip}{port}"
+            x_urls.append(x_url)
+        urls = set(x_urls)  # 去重得到唯一的URL列表
+    
+        # Writing unique URLs to a file
+       output_file_path = "tv/shen.txt"
+
+with open(output_file_path, "w") as output_file:
+    for unique_url in urls:
+        output_file.write(f"{unique_url}\n")
+
+print("Unique URLs have been written to 'tv/shen.txt' successfully.")
